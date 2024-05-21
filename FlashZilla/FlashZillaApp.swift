6c3fd67e122b0bc1121ea33c5bd2bd883756697c
bvo24 <brianvo2020@gmail.com>
2024-05-21 14:26:11 -0400

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Brian Vo on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
